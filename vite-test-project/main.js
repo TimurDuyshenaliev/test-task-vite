32f04f0467d2173369b388cc1ed756c900654395
Timur Duishenaliiev <timaster999@gmail.com>
2023-12-02 22:50:44 +0200

    done

--- vite-test-project/main.js
+++ vite-test-project/main.js
@@ -2,3 +2,25 @@ function toggleMenu() {
     const menu = document.querySelector('.header__nav');
     menu.classList.toggle('menu-open');
   }
+  var swiper = new Swiper(".mySwiperThumb", {
+    loop: true,
+    slidesPerView: 3,
+    direction: "vertical",
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiperMain", {
+    loop: true,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
